902692d06ee3961026ad898e186fc845adc3b6f1
Eduardo Sacramento de Sousa <eduardo.sacra.sousa@gmail.com>
2018-10-07 18:43:14 -0300

    [INC] Adicionado diretiva meu painel

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -1,1 +1,20 @@
-angular.module('minhasDiretivas',[]);
+angular.module('minhasDiretivas',[])
+.directive('meuPainel', function(){
+    
+    //directive directive object
+    var ddo = {};
+    ddo.restric = "AE";
+
+    ddo.scope = {
+        titulo: '@'
+    };
+
+    //matem os elementos filhos quando carregar o html
+    //para funcionar é preciso marcar qual elemento ira manter os elementos filhos 
+    //com a diretiva ng-transclude
+    ddo.transclude = true;
+
+    ddo.templateUrl = 'js/directives/meu-painel.html';
+
+    return ddo;
+});
